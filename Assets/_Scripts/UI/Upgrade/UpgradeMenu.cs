405b712c758b9d757ab1a84278575fe20fa75135
Vseslav <Vseslav2244@gmail.com>
2023-02-22 17:34:29 +0300

    UI update, abilitiesPanel

--- Assets/_Scripts/UI/Upgrade/UpgradeMenu.cs
+++ Assets/_Scripts/UI/Upgrade/UpgradeMenu.cs
@@ -1,5 +1,7 @@
 ﻿using _Scripts.UI.Buttons.Shop_Buttons;
+using _Scripts.UI.Windows;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Scripts.UI.Upgrade
 {
@@ -10,6 +12,14 @@ namespace _Scripts.UI.Upgrade
         [SerializeField] private UpgradeButton rangeUpgrade;
         [SerializeField] private UpgradeButton towerHealth;
         [SerializeField] private UpgradeButton towerLevel;
+        
+        [Space(10)]
+        [SerializeField] private Button openAbilitiesButton;
+        [SerializeField] private Button closeAbilitiesButton;
+        
+        [Space(10)]
+        [SerializeField] private CanvasGroup upgradePanel;
+        [SerializeField] private CanvasGroup abilitiesPanel;
         #endregion
 
         #region Properties
@@ -17,5 +27,23 @@ namespace _Scripts.UI.Upgrade
         public float TowerHealth => towerHealth.Coefficient;
         public float TowerLevel => towerLevel.Coefficient;
         #endregion
+        
+        private void Awake()
+        {
+            openAbilitiesButton.onClick.AddListener(() =>
+            {
+                ShopWindowSwipe(true);
+            });
+            closeAbilitiesButton.onClick.AddListener(() =>
+            {
+                ShopWindowSwipe(false);
+            });
+        }
+        
+        private void ShopWindowSwipe(bool isAbilitiesPanel)
+        {
+            WindowsManager.CanvasGroupSwap(abilitiesPanel, isAbilitiesPanel);
+            WindowsManager.CanvasGroupSwap(upgradePanel, !isAbilitiesPanel);
+        }
     }
 }
