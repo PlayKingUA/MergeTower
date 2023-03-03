cb574a27cc2987fdd8a3bacbc2f773a9a3536ea6
Vseslav <Vseslav2244@gmail.com>
2023-03-03 19:56:45 +0300

    Abilities panel UI progress

---
+++ Assets/_Scripts/UI/Buttons/AbilityButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Scripts.UI.Buttons
+{
+    [RequireComponent(typeof(Toggle))]
+    public class AbilityButton : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI nameText;
+        
+        [SerializeField] private string abilityName;
+        [SerializeField, TextArea] private string description;
+        [Space(10)]
+        [SerializeField] private int requiredTowerLevel;
+        
+        private Toggle _toggle;
+        
+        private void Awake()
+        {
+            _toggle = GetComponent<Toggle>();
+            //_toggle.onValueChanged += 
+        }
+
+        private void OnValidate()
+        {
+            nameText.text = abilityName;
+        }
+    }
+}
---
+++ Assets/_Scripts/UI/Upgrade/AbilitiePanel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AbilitiePanel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI abilityNameText;
+    [SerializeField] private TextMeshProUGUI abilityDescriptionText;
+    
+    
+    
+}
